2cc24cac6ee8251f25cf02de3ae3ba9f119d6fa3
Hu Wentao <huwentaocn@gmail.com>
2023-11-06 13:34:48 +0800

    首次提交

---
+++ src/main/java/com/hwt/template/SpringCloudCodeTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.hwt.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudCodeTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudCodeTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hwt/template/SpringCloudCodeTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwt.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudCodeTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
